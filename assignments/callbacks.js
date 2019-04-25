0951e9ec27e9eeb558e4530f01c41a301159915a
Kevin-S <kevsilvo@gmail.com>
2019-04-25 15:50:49 -0400

    callback v1

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,17 +24,28 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+/////////////////////////////////// getLength passes the length of the array into the callback.
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+getLength(items, function(length){
+  console.log(length);
+});
+
+
+ ///////////////////////////////// last passes the last item of the array into the callback.
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+ 
+  return cb(arr[`${arr.length - 1}`]);
 }
+last(items,function(lastItemOfArray){
+  console.log(lastItemOfArray);
+});
 
+ ////////////////////////////////// sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+ 
 }
 
 function multiplyNums(x, y, cb) {
